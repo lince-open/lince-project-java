a8c2764bd4749c434cd5ba1990a2d9bca2b4f35d
Charles6425 <77788041+Charles6425@users.noreply.github.com>
2021-04-22 16:54:08 -0300

    Update ProjectController.java

--- src/main/java/work/lince/project/controller/ProjectController.java
+++ src/main/java/work/lince/project/controller/ProjectController.java
@@ -1,16 +1,12 @@
 package work.lince.project.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import work.lince.commons.log.LogExecutionTime;
 import work.lince.project.model.Project;
 import work.lince.project.service.ProjectService;
@@ -20,30 +16,38 @@ import java.util.List;
 @LogExecutionTime
 @RestController
 @RequestMapping(path = "/projects")
+@Api(value = "Lince Project")
+@CrossOrigin(origins = "*")
 public class ProjectController {
 
     @Autowired
     protected ProjectService service;
 
     @PostMapping
+    @ApiOperation(value = "Cadastra um novo projeto")
     @ResponseStatus(HttpStatus.CREATED)
     public Project create(@RequestBody @Validated Project body) {
         return service.create(body);
     }
 
     @GetMapping
+    @ApiOperation(value = "Retorna uma lista com todos os projetos")
     @ResponseStatus(HttpStatus.OK)
     public List<Project> findAll() {
         return service.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}")
+    @ApiOperation(value = "Retorna um projeto")
     @ResponseStatus(HttpStatus.OK)
-    public Project findById(@PathVariable("id") final Long id) {
-        return service.findById(id);
+    public ResponseEntity<Project> findById(@PathVariable("id") final Long id) {
+        Project obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
+
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Exclui um projeto")
     @ResponseStatus(HttpStatus.OK)
     public void remove(@PathVariable("id") final Long id) {
         service.remove(id);
